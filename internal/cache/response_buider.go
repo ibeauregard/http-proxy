33d255e0af5523fd99610a0cf055f9b0e33bca4b
Ian Beauregard <ian.a.beauregard@pm.me>
2022-09-11 22:07:27 -0400

    Refactor cacheResponseBuilder (shorter functions)

--- internal/cache/response_buider.go
+++ internal/cache/response_buider.go
@@ -13,7 +13,7 @@ import (
 type cacheResponseBuilder struct {
 	response *http_.Response
 	reader   *cacheEntryReader
-	error    bool
+	err      error
 }
 
 func newCacheResponseBuilder(readCloser io.ReadCloser) *cacheResponseBuilder {
@@ -28,47 +28,26 @@ func newCacheResponseBuilder(readCloser io.ReadCloser) *cacheResponseBuilder {
 func (b *cacheResponseBuilder) setStatusCode() *cacheResponseBuilder {
 	firstLine, err := getLine(b.reader)
 	if err != nil {
-		return b.withErrorStatus()
+		return b.withError(err)
 	}
 	b.response.StatusCode, err = getStatusCode(firstLine)
-	b.error = err != nil
-	return b
+	return b.withError(err)
 }
 
 var headerMatchingRegexp = regexp.MustCompile(`([-\w]+)\s*:\s*(.*\S)`)
 
 func (b *cacheResponseBuilder) setHeaders() *cacheResponseBuilder {
-	if b.error {
+	if b.err != nil {
 		return b
 	}
-	line, err := b.reader.ReadString('\n')
-	if err != nil {
-		errors.Log(b.setHeaders, err)
-		return b.withErrorStatus()
+	if err := b.setCachedHeaders(); err != nil {
+		return b.withError(err)
 	}
-	b.response.Header = make(map[string][]string)
-	for line != "\r\n" {
-		err = setHeader(b.response.Header, line)
-		if err != nil {
-			errors.Log(b.setHeaders, err)
-			return b.withErrorStatus()
-		}
-		line, err = b.reader.ReadString('\n')
-		if err != nil {
-			errors.Log(b.setHeaders, err)
-			return b.withErrorStatus()
-		}
-	}
-	err = addAgeHeader(b.response.Header)
-	if err != nil {
-		errors.Log(b.setHeaders, err)
-		return b.withErrorStatus()
-	}
-	return b
+	return b.withError(b.setAgeHeader())
 }
 
 func (b *cacheResponseBuilder) setBody() *cacheResponseBuilder {
-	if b.error {
+	if b.err != nil {
 		return b
 	}
 	b.response.Body = &http_.Body{ReadCloser: b.reader}
@@ -76,35 +55,61 @@ func (b *cacheResponseBuilder) setBody() *cacheResponseBuilder {
 }
 
 func (b *cacheResponseBuilder) build() *http_.Response {
-	if b.error {
+	if b.err != nil {
 		return nil
 	}
 	return b.response
 }
 
-func (b *cacheResponseBuilder) withErrorStatus() *cacheResponseBuilder {
-	b.error = true
+func (b *cacheResponseBuilder) setCachedHeaders() error {
+	line, err := getLine(b.reader)
+	if err != nil {
+		return err
+	}
+	b.response.Header = make(map[string][]string)
+	for line != "\r\n" {
+		if err = setHeader(b.response.Header, line); err != nil {
+			errors.Log(b.setHeaders, err)
+			return err
+		}
+		if line, err = getLine(b.reader); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (b *cacheResponseBuilder) setAgeHeader() error {
+	if err := addAgeHeader(b.response.Header); err != nil {
+		errors.Log(b.setHeaders, err)
+		return err
+	}
+	return nil
+}
+
+func (b *cacheResponseBuilder) withError(err error) *cacheResponseBuilder {
+	b.err = err
 	return b
 }
 
-type byteSliceReader interface {
-	ReadBytes(delim byte) ([]byte, error)
+type stringReader interface {
+	ReadString(byte) (string, error)
 }
 
-func getLine(reader byteSliceReader) ([]byte, error) {
-	line, err := reader.ReadBytes('\n')
+func getLine(reader stringReader) (string, error) {
+	line, err := reader.ReadString('\n')
 	if err != nil {
 		errors.Log(getLine, errors.New("unexpected end of cache entry"))
-		return nil, err
+		return "", err
 	}
 	return line, nil
 }
 
 var statusCodeRegexp = regexp.MustCompile(`\b\d{3}\b`)
 
-func getStatusCode(firstLine []byte) (int, error) {
-	decimalStatusCode := statusCodeRegexp.Find(firstLine)
-	statusCode, err := strconv.Atoi(string(decimalStatusCode))
+func getStatusCode(firstLine string) (int, error) {
+	decimalStatusCode := statusCodeRegexp.FindString(firstLine)
+	statusCode, err := strconv.Atoi(decimalStatusCode)
 	if err != nil {
 		errors.Log(getStatusCode, errors.New(
 			"first line of the cache entry does not contain a valid HTTP response status code"))
