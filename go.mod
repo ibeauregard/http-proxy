36ea104d0e2e6ab7838691b2777b2b7e77131a6a
Ian Beauregard <ian.a.beauregard@pm.me>
2022-11-26 01:03:06 -0500

    Setup the shutdown handling (persist index to disk)

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module my_proxy
 
 go 1.19
+
+require github.com/ztrue/shutdown v0.1.1
--- internal/main.go
+++ internal/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
+	"fmt"
+	"github.com/ztrue/shutdown"
 	"log"
 	"net/http"
+	"syscall"
 )
 
 func main() {
-	log.Panic(http.ListenAndServe(":8080", http.HandlerFunc(myProxy)))
+	shutdown.Add(func() {
+		fmt.Println("Shutdown!")
+	})
+	go func() {
+		fmt.Println("Proxy listening on http://localhost:8080")
+		log.Panic(http.ListenAndServe(":8080", http.HandlerFunc(myProxy)))
+	}()
+	shutdown.Listen(syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
 }
